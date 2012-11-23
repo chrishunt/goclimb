41c0cc8eb647a8219e01430d7645464536af4e72
Chris Hunt and Nick Hunt <github+c+nb@squareup.com>
2012-11-22 16:51:08 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goclimb::Application.config.secret_token = '4d6dac0097b11fee5ba888870bc562dc2f2da7819ada994cdec70b3c5266ce6c67bcdc7247106a7e5d8378f2f7ba6fa54344768242e573edcad629d3403a869e'
